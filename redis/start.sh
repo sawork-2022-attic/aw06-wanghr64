3b53f3641c9b1e5ed139c465e76b8f14a12679ca
wanghr <239955612@qq.com>
2022-05-25 19:08:25 +0800

    add redit

---
+++ redis/start.sh
@@ -0,0 +1,3 @@
+for d in ./*/ ; do (cd "$d" && redis-server ./redis.conf &); done
+
+redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1
