9e3ecf05754ea08981d92a03ef38baaced666c8f
wanghr <239955612@qq.com>
2022-05-26 10:13:32 +0800

    report

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # 测试环境
 
-- 操作系统：Windows 10
+- 操作系统：ArchLinux
 - Java版本：openjdk version "11.0.15" 2022-04-19 LTS
-- SQL：Ver 15.1 Distrib 10.7.3-MariaDB, for Win64 (AMD64)
+- Maven版本：Apache Maven 3.8.4
+- SQL：mysql  Ver 15.1 Distrib 10.7.4-MariaDB, for Linux (x86_64) using readline 5.1
 - 开发工具：Visual Studio Code
 
 # 基本批处理和与SQL的整合
 
+使用`JsonReader`类对json文件进行读取，`ProductWriter`类将`Product`类写入到数据库中。
 
+使用`PartitionConfig`类对批处理任务进行设置，考虑到测试机器CPU为8c16t，便将线程池的大小设置为`32`。
 
-![](https://www.png8.com/imgs/2022/05/18/89c1a9f1e51796e8.png)
+最终得到mysql中的查询结果如下：
+
+![](./pics/SQLresult.png)
+
+# 与webpos的整合
+
+参考aw04中的`JD.java`，定义类`AMZ`对接口`PosDB`进行实现。
+
+`getProducts`函数通过JDBC访问数据库，为了效率考虑，执行的mysql语句为加入了`LIMIT 500`。
+
+最终得到webpos的界面如下：
+
+![](./pics/webpos.png)
--- webpos/src/main/java/com/example/webpos/db/AMZ.java
+++ webpos/src/main/java/com/example/webpos/db/AMZ.java
@@ -28,6 +28,8 @@ public class AMZ implements PosDB {
 
     private Random priceGenerator;
 
+    private ResultSet rs;
+
     public AMZ() {
         System.out
                 .print("\n\n\n\n\n\n\n\n\n\n\n\nsssssssssssssssssssssssssasdasdasdasdasdasdasd\n\n\n\n\n\n\n\n\n\n\n");
@@ -35,6 +37,7 @@ public class AMZ implements PosDB {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             priceGenerator = new Random();
+            rs = conn.createStatement().executeQuery("SELECT * FROM amazon LIMIT 500;");
         } catch (Exception e) {
             System.out.println("\n\n\n\n\n\n\n\n\n\n\n\nFailed to connect.\n\n\n\n\n\n\n\n\n\n\n\n");
         }
@@ -46,12 +49,11 @@ public class AMZ implements PosDB {
         System.out.println("Try to get Products.");
         products = new ArrayList<>();
         try {
-            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM amazon;");
-            // int count = 0;
+
             while (rs.next()) {
                 products.add(new Product(rs.getString("asin"), rs.getString("title"), priceGenerator.nextInt(20) + 10,
                         rs.getString("imageURLHighRes")));
-                // ++count;
+
             }
         } catch (Exception e) {
             System.out.println("Failed to get Products.");
